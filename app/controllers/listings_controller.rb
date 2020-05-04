7fa78ce8edf876c2f2bfed375d6570839cf1d85c
hai14894 <hohoanghai14894@gmail.com>
2020-05-03 20:49:37 -0700

    added show view

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,28 @@
+class ListingsController < ApplicationController
+    before_action :authenticate_user!
+    def index
+        @listings = Listing.all
+    end
+    def new
+    @listing = Listing.new
+    end
+    def  create     
+        @listing = current_user.listings.create(listing_params)
+        if @listing.error.any?
+            
+            render "new"
+        else
+        
+        redirect_to listings_path
+        end
+
+    end
+    def show 
+        @listing = Listing.find(params["id"])
+    end
+
+private
+    def listing_params 
+        params.require(:listing).permit(:title, :description, :price, :picture)
+    end
+end
